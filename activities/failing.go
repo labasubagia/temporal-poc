8e87cc4740d2f6926a506ebd685ad93d563892b0
Laba Subagia <labasubagia22@gmail.com>
2026-05-09 17:24:32 +0800

    refactor: remove dynamic delay function and use fixed delay for activity tasks

--- activities/failing.go
+++ activities/failing.go
@@ -3,7 +3,6 @@ package activities
 import (
 	"context"
 	"errors"
-	"os"
 	"time"
 )
 
@@ -13,31 +12,23 @@ func NewFailingActivities() *FailingActivities {
 	return &FailingActivities{}
 }
 
-func getDelay() time.Duration {
-	if v := os.Getenv("ACTIVITY_DELAY"); v != "" {
-		if d, err := time.ParseDuration(v); err == nil {
-			return d
-		}
-	}
-	return 500 * time.Millisecond
-}
 
 func (a *FailingActivities) TaskOne(ctx context.Context, id string) error {
-	time.Sleep(getDelay())
+	time.Sleep(1 * time.Second)
 	return nil
 }
 
 func (a *FailingActivities) TaskTwo(ctx context.Context, id string) error {
-	time.Sleep(getDelay())
+	time.Sleep(1 * time.Second)
 	return errors.New("task two failed")
 }
 
 func (a *FailingActivities) TaskThree(ctx context.Context, id string) error {
-	time.Sleep(getDelay())
+	time.Sleep(1 * time.Second)
 	return nil
 }
 
 func (a *FailingActivities) TaskFour(ctx context.Context, id string) error {
-	time.Sleep(getDelay())
+	time.Sleep(1 * time.Second)
 	return nil
 }
